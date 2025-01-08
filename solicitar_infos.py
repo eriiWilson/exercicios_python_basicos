ce588555242cd3ad4da89b908008bdbcceb6e867
eriiWilson <eric_wils0n@outlook.com>
2025-01-08 16:44:42 -0300

    exercicios de solicitação de infos em python

---
+++ solicitar_infos.py
@@ -0,0 +1,6 @@
+#Solicitar informações de usuario e exibir elas
+Nome = input('Qual o seu nome? ')
+Idade = int(input('quantos anos você tem? '))
+Altura = float(input('qual sua altura? '))
+
+print(f'Seu nome é {Nome} sua idade é {Idade} e sua altura é {Altura} ')
